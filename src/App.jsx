c2a22ec451890d51a48dca7a437841b128b487f0
Abdul Basit <abdulbasitbasit076@gmail.com>
2023-06-09 22:38:03 +0500

    New Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { TodoList } from './todolist/TodoList'
+import "./App.css"
+import { TodoList } from "./todolist/TodoList"
 
 function App() {
-      return <TodoList />
+  return <TodoList />
 }
 
 export default App
--- src/todolist/TodoItem.jsx
+++ src/todolist/TodoItem.jsx
@@ -1,11 +1,23 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react"
 import DeleteIcon from "../assets/deleteicon.png"
 import "./todo.css"
 
-export const TodoItem = ({title}) => {
-    return (
-        <div className="todo-item" id="todo-1">
-            <p className="todo-task-title">{title}</p>
-            <img src={DeleteIcon} className="todo-icon" />
-        </div>
-    )
+export const TodoItem = ({ title, deleteTodo, findTitleById}) => {
+  const [color,setcolor] = useState (['black'])
+  const [underlined,setunderlined] = useState([])
+
+  const clickcolor = () => {
+    setcolor('blue')
+    setunderlined('underline')
+  }
+  return (
+    <div className="todo-item" id="todo-1">
+      <p className="todo-task-titl" style={{color: color, textDecoration: underlined}}>{title}</p>
+      <div>
+      <button className="todo-check-btn" onClick={clickcolor}>&#x2713;</button>  
+      <img className="todo-icon" src={DeleteIcon} onClick={deleteTodo} />
+    </div>
+    </div>
+  )
 }
